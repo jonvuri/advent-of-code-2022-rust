50814effc8823c05c6cfabda1ac7c232c4f2ac82
Jon Rajavuori <jrajav@gmail.com>
2022-12-10 13:25:50 -0600

    Add solution for day 2 part 1

---
+++ src/day2.rs
@@ -0,0 +1,53 @@
+use aoc_parse::{parser, prelude::*};
+
+use crate::utils;
+
+pub fn generator(input: &str) -> Vec<(char, char)> {
+    parser!(lines(alpha " " alpha))
+        .parse(&utils::newline_end(&input))
+        .unwrap()
+}
+
+pub fn part1(input: &[(char, char)]) -> Result<u32, &'static str> {
+    input
+        .iter()
+        .map(|line| match line {
+            ('A', 'X') => Ok(1 + 3),
+            ('A', 'Y') => Ok(2 + 6),
+            ('A', 'Z') => Ok(3),
+
+            ('B', 'X') => Ok(1),
+            ('B', 'Y') => Ok(2 + 3),
+            ('B', 'Z') => Ok(3 + 6),
+
+            ('C', 'X') => Ok(1 + 6),
+            ('C', 'Y') => Ok(2),
+            ('C', 'Z') => Ok(3 + 3),
+
+            _ => Err("Invalid input"),
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::generator;
+
+    use super::part1;
+
+    static EXAMPLE_INPUT: &str = "A Y\nB X\nC Z\n";
+
+    #[test]
+    fn parses() {
+        assert_eq!(
+            generator(EXAMPLE_INPUT),
+            vec![('A', 'Y'), ('B', 'X'), ('C', 'Z')]
+        );
+    }
+
+    #[test]
+    fn part1_example() {
+        let input = generator(EXAMPLE_INPUT);
+        assert_eq!(part1(&input), Ok(15));
+    }
+}
