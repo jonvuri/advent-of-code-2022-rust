a119c692d9013211faaec5140f6110dfb9b537f1
Jon Rajavuori <jrajav@gmail.com>
2022-12-10 04:43:33 -0600

    Day 1 part 2 solution

--- src/day1.rs
+++ src/day1.rs
@@ -12,11 +12,20 @@ pub fn part1(input: &Vec<Vec<u32>>) -> u32 {
     input.iter().map(|group| group.iter().sum()).max().unwrap()
 }
 
+pub fn part2(input: &Vec<Vec<u32>>) -> u32 {
+    use std::collections::BinaryHeap;
+
+    let mut heap = BinaryHeap::<u32>::from_iter(input.iter().map(|group| group.iter().sum()));
+
+    heap.pop().unwrap() + heap.pop().unwrap() + heap.pop().unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::day1::generator;
 
     use super::part1;
+    use super::part2;
 
     #[test]
     fn parses() {
@@ -25,8 +34,14 @@ mod tests {
     }
 
     #[test]
-    fn finds_max() {
+    fn part1_finds_max() {
         let input: Vec<Vec<u32>> = vec![vec![100, 200], vec![100, 100]];
         assert_eq!(part1(&input), 300);
+    }
+
+    #[test]
+    fn part2_sums_top_3() {
+        let input: Vec<Vec<u32>> = vec![vec![100, 200], vec![100, 100], vec![250, 100]];
+        assert_eq!(part2(&input), 850);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod day1;
 
 aoc_main::main! {
     year 2022;
-    day1 : generator => part1;
+    day1 : generator => part1, part2;
 }
