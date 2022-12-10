d5ef54feff1a93dc5a61033ad641cb75104a988f
Jon Rajavuori <jrajav@gmail.com>
2022-12-10 13:25:10 -0600

    Rearrange day1 tests

--- src/day1.rs
+++ src/day1.rs
@@ -9,14 +9,15 @@ pub fn generator(input: &str) -> Vec<Vec<u32>> {
 }
 
 pub fn part1(input: &Vec<Vec<u32>>) -> u32 {
-    let elves = input.iter().map(|group| group.iter().sum());
+    let elves = input.iter().map(|section| section.iter().sum());
+
     elves.max().unwrap()
 }
 
 pub fn part2(input: &Vec<Vec<u32>>) -> u32 {
     use std::collections::BinaryHeap;
 
-    let elves = input.iter().map(|group| group.iter().sum());
+    let elves = input.iter().map(|section| section.iter().sum());
 
     // BinaryHeap is a max-heap by default
     let mut heap = BinaryHeap::<u32>::from_iter(elves);
@@ -31,21 +32,27 @@ mod tests {
     use super::part1;
     use super::part2;
 
+    static EXAMPLE_INPUT: &str = "100\n200\n\n100\n100\n\n250\n100";
+
     #[test]
-    fn parses() {
-        let input = "100\n200\n\n100\n100";
-        assert_eq!(generator(&input), vec![vec![100, 200], vec![100, 100]]);
+    fn parses_example() {
+        assert_eq!(
+            generator(EXAMPLE_INPUT),
+            vec![vec![100, 200], vec![100, 100], vec![250, 100]]
+        );
     }
 
     #[test]
     fn part1_finds_max() {
-        let input: Vec<Vec<u32>> = vec![vec![100, 200], vec![100, 100]];
-        assert_eq!(part1(&input), 300);
+        let input = generator(EXAMPLE_INPUT);
+
+        assert_eq!(part1(&input), 350);
     }
 
     #[test]
     fn part2_sums_top_3() {
-        let input: Vec<Vec<u32>> = vec![vec![100, 200], vec![100, 100], vec![250, 100]];
+        let input = generator(EXAMPLE_INPUT);
+
         assert_eq!(part2(&input), 850);
     }
 }
