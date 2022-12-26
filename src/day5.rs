a65dba0739602ad02b2906f0792c3a49b261a26f
Jon Vuri <jrajav@gmail.com>
2022-12-25 19:45:54 -0600

    Add day 5 part 1 solution

---
+++ src/day5.rs
@@ -0,0 +1,103 @@
+use aoc_parse::{parser, prelude::*};
+
+use crate::utils;
+
+#[derive(Debug)]
+struct Move {
+    quantity: usize,
+    source: usize,
+    target: usize,
+}
+
+#[derive(Debug)]
+pub struct Input {
+    stacks: Vec<Vec<char>>,
+    moves: Vec<Move>,
+}
+
+pub fn generator(input: &str) -> Input {
+    let stack_parser = parser!(
+        rows:lines(
+            repeat_sep(
+                {
+                    "   " => None,
+                    "[" ch:alpha "]" => Some(ch),
+                },
+                " "
+            )
+        )
+        =>
+        {
+            let length = rows[0].len();
+            let mut stacks: Vec<Vec<char>> = vec![Vec::with_capacity(64); length];
+            for row in rows.iter().rev() {
+                for (i, ch) in row.iter().enumerate() {
+                    if let Some(character) = ch {
+                        stacks[i].push(*character);
+                    }
+                }
+            }
+            stacks
+        }
+    );
+
+    let move_parser = parser!(
+        "move " quantity:usize " from " source:usize " to " target:usize
+            =>
+            Move { quantity, source: source - 1, target: target - 1 }
+    );
+
+    parser!(
+        stacks:stack_parser
+        line(repeat_sep(" " digit " ", " "))
+        line("")
+        moves:lines(move_parser)
+        => {
+            Input { stacks, moves }
+        }
+    )
+    .parse(&utils::newline_end(input))
+    .unwrap()
+}
+
+pub fn part1(input: &Input) -> String {
+    let mut stacks = input.stacks.clone();
+
+    for Move {
+        quantity,
+        source,
+        target,
+    } in input.moves.iter()
+    {
+        for _ in 0..*quantity {
+            let ch = stacks[*source].pop().unwrap();
+            stacks[*target].push(ch)
+        }
+    }
+
+    stacks
+        .into_iter()
+        .map(|stack| stack[stack.len() - 1])
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{generator, part1};
+
+    static EXAMPLE_INPUT: &str = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+    #[test]
+    fn part1_example() {
+        let input = generator(EXAMPLE_INPUT);
+        assert_eq!(part1(&input), "CMZ");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 aoc_main::main! {
     year 2022;
@@ -11,4 +12,5 @@ aoc_main::main! {
     day2 : generator => part1?, part2?;
     day3 : generator => part1?, part2?;
     day4 : generator => part1;
+    day5 : generator => part1;
 }
